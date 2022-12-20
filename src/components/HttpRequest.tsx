da50c96ed6740e11fd6f78114144103d849c2ca6
Eug <187samp@gmail.com>
2022-12-20 19:45:18 +0200

    Post request

--- src/components/HttpRequest.tsx
+++ src/components/HttpRequest.tsx
@@ -6,23 +6,37 @@ interface Post {
   id: number;
 }
 function HttpRequest() {
-  const baseUrl = "https://jsonplaceholder.typicode.com/users/1/";
+  const baseUrl = "https://jsonplaceholder.typicode.com/";
   const [posts, setPosts] = useState<Array<Post>>([]);
 
   useEffect(() => {
-    axios.get(`${baseUrl}posts`).then((response) => {
+    axios.get(`${baseUrl}users/1/posts`).then((response) => {
       setPosts(response.data);
     });
   }, []);
 
+  function createPost() {
+    axios
+      .post(`${baseUrl}posts`, {
+        title: "New post title",
+        body: "New post body",
+      })
+      .then((res) => {
+        setPosts([...posts, res.data]);
+      });
+  }
+
   if (posts.length === 0) return <div>No posts</div>;
 
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
-    </div>
+    <>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>{post.title}</div>
+        ))}
+      </div>
+      <button onClick={createPost}>Create Post</button>
+    </>
   );
 }
 
